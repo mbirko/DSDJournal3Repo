558e9ca92570e47de73154a9b3169b2017040323
mbirko <35766676+mbirko@users.noreply.github.com>
2021-10-28 10:46:45 +0200

    Added Exersize 5 task 2

---
+++ ExerciseProjects/2/guess_game.vhd
@@ -0,0 +1,21 @@
+
+
+library ieee;
+use ieee.std_logic_1164.all;
+
+entity guess_game is
+	port
+	(
+		inputs : in_std_logic_vector
+		-- Input ports
+		<name>	: in  <type>;
+		<name>	: in  <type> := <default_value>;
+
+		-- Inout ports
+		<name>	: inout <type>;
+
+		-- Output ports
+		<name>	: out <type>;
+		<name>	: out <type> := <default_value>
+	);
+end guess_game;
